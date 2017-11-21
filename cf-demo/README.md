16bc85df5d57595c502e8639f1915e8feb22bff6
sleyzerzon <senya22@yahoo.com>
2017-11-21 11:58:16 -0500

    cf-demo project

--- cf-demo/README.md
+++ cf-demo/README.md
@@ -7,3 +7,5 @@ export PS1="$"
 alias idea="open -a /Applications/IntelliJ\ IDEA\ CE.app"
 alias ll='ls -lG'
 ~~~
+
+/Users/inrhythm_058/Desktop/Screen Shot 2017-11-21 at 11.54.33 AM.png
--- cf-demo/src/main/java/com/example/cfdemo/CfDemoApplication.java
+++ cf-demo/src/main/java/com/example/cfdemo/CfDemoApplication.java
@@ -2,11 +2,22 @@ package com.example.cfdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CfDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CfDemoApplication.class, args);
+	}
+
+	@RestController
+	class GreetingsRestController{
+
+		@GetMapping("/hi")
+		String hi(){
+			return "Hello, world!";
+		}
 	}
 }
